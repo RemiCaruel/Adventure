a807f065a57ce04c21d6b648d196b85448fae9c2
RCaruel <remi.caruel@isep.fr>
2022-04-18 14:59:17 +0200

    Adding Map Scalability
    needs improvement but handles map sizes that are too big

--- client/src/component/MapViewer.js
+++ client/src/component/MapViewer.js
@@ -10,6 +10,8 @@ var regExPlayer   = /A[ ]*-[ ]*([a-zA-Z]+)[ ]*-[ ]*([0-9]+)[ ]*-[ ]*([0-9]+)[ ]*
 
 function drawMap(cmds) {
     document.getElementById("MapViewerContainer").innerHTML = ReactDOMServer.renderToString(CreateMap(cmds[0]));
+    setCoefficientSize(1);
+    AdaptMapSize(regExMap.exec(cmds[0])[1], regExMap.exec(cmds[0])[2]);
     for (let i = 1; i < cmds.length; i++) {
         if (regExMountain.test(cmds[i])) {
             let elementInfo = regExMountain.exec(cmds[i]);
@@ -32,10 +34,28 @@ function drawMap(cmds) {
     }
 }
 
+function AdaptMapSize(x, y) {
+    let mapHolderRef = document.getElementsByClassName("MapHolder")[0];
+    let mapViewerContainerRef = document.getElementById("MapViewerContainer");
+    if (mapHolderRef.clientWidth < mapViewerContainerRef.clientWidth
+        &&
+        mapHolderRef.clientHeight < mapViewerContainerRef.clientHeight) return;
+    
+    let coeff = Math.max(
+        (mapHolderRef.clientWidth + x*2) / (mapViewerContainerRef.clientWidth),
+        (mapHolderRef.clientHeight + y*2) / (mapViewerContainerRef.clientHeight)
+    )
+    setCoefficientSize(coeff);
+}
+
+function setCoefficientSize(coeff) {
+    document.documentElement.style.setProperty('--coefficient', coeff);
+}
+
 function CreateMap(cmd) {
-    return Array.apply(null, Array(parseInt(regExMap.exec(cmd)[1]))).map((idx) => {
+    return <div className="MapHolder">{Array.apply(null, Array(parseInt(regExMap.exec(cmd)[1]))).map((idx) => {
         return <Row amount={parseInt(regExMap.exec(cmd)[2])} />;
-    })
+    })}</div>;
 }
 
 const Row = (props) => {
